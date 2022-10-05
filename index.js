c77664cfd8ae374b66d4f2b4b737c5845ccbbea8
Gerald <kapgerado05@gmail.com>
2022-10-05 15:02:29 +0300

    Done with JS array find method lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+const superbowlWin = (record) => {
+    let denverBroncos = record.find(log => {
+        if(log.result === "W"){
+            return log;
+        }
+    })
+    if(denverBroncos){
+      return denverBroncos.year;
+    }else{
+      return denverBroncos
+    }
+    console.log(`Denver Broncos won the superbowl $find()`)
+}
